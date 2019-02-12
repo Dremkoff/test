6a2bcde5ef79b674eae6eef14a63ca989946bdd6
Dremkoff <n.dremkoff@gmail.com>
2019-02-12 23:47:50 +0300

    Created default components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import Header from './components/header';
+import Login from './components/login';
+import Profile from './components/profile';
+
+const App = () => (
+  <div>
+    <Header />
+    <Login />
+    <Profile />
+  </div>
+);
+
+export default App;
